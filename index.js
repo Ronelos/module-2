722be01309d1311877c7409fcb9813c338b86578
Andrii Kryvyi <1ronelos1@gmail.com>
2024-11-04 21:39:23 +0200

    work 1

---
+++ index.js
@@ -0,0 +1,43 @@
+//  const user = Number(prompt('Введіть число 10'));
+
+//  if (user === 10) {
+//     console.log ('вірно');
+//  } else {
+//  console.log ('не вірно');
+// }
+
+/////////////////////////////////////////////////////
+
+// const daysUnit = 2;
+// if (daysUnit === 0) {
+//     console.log ('Today');
+
+// } else if (daysUnit === 1) {
+//     console.log ('Tomorrow');
+
+// } else if (daysUnit === 2) {
+//     console.log ('TOvermorow');
+    
+// } else if (daysUnit >= 3) {
+//     console.log ('Date in the future');
+    
+// }
+
+///////////////////////////////////////
+
+// const a = 1;
+// const b = 1;
+// let result = a + b < 4 ? 'malo': 'bagato';
+
+// console.log (result);
+
+//////////////////////////////////////////
+
+
+
+
+
+
+
+
+
